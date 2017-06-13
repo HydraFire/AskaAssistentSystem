2ef0bcad96e260a7505dd23ba3631264fee27384
HydraFire <hydrafire.plus@gmail.com>
2017-06-13 12:09:41 +0300

    first add files

---
+++ main.js
@@ -0,0 +1,98 @@
+
+const electron = require('electron');
+//const jetpack = require('fs-jetpack');
+const app = electron.app;  // Module to control application life.
+app.on('window-all-closed', () => {
+  console.log('не закрывает сервер после закрытия окна')
+})
+const BrowserWindow = electron.BrowserWindow;
+const jetpack = require('fs-jetpack');
+const windowManager = require('electron-window-manager');
+
+const debug = function(e){
+  jetpack.write('debag.txt',e)
+}
+exports.debug = debug;
+debug('проверка')
+
+
+
+//Обявление Mesengera //////////////////////////////////////////
+const sendToAska = function (message,ws){
+  ws.send(message)
+}
+exports.sendToAska = sendToAska;
+//Подключение функции работаюшей с нейросетью ////////////////////
+const set_to_run = require('./neural_network').set_to_run;
+const calc_layers = require('./neural_network').calc_layers;
+//////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+function somebodyConnected(ws){
+  let message = 'SYSTEM';
+  let userInfo = 'Ваш пользователь: '+ws.x_user;
+  message += userInfo+'<br><br>'
+  let version = 'Текущая версия программы 0.301<br>'
+  message += version+'<br>'
+  let brainSize = jetpack.inspect('NN_train_buffer.json').size
+  let arrL = calc_layers()
+  message += '<br>'
+  message += '[ входящий слой:  '+arrL[0]+'  ]<br>'
+  message += '[ внутрених нейронов:  '+arrL[1]+'  ]<br>'
+  message += '[ выходяший слой:  '+arrL[2]+'  ]<br>'
+  message += '<br>'
+  message += 'Размер нейро сети '+(brainSize/1000000).toFixed(2)+' МБ.'
+  ws.send(message)
+}
+//// WebSocketServer ///////////////////////////////////////////////
+var WebSocketServer = require("ws").Server,
+    express = require("express"),
+    http = require("http"),
+    exp = express(),
+    server = http.createServer(exp);
+
+exp.use(express.static(__dirname + '/public'));
+server.listen(8080);
+function somebodyConnected_log(ws,id,message){
+  let message2 = 'SYSTEM';
+  
+  message2 += JSON.stringify(id)+'  '+message;
+  ws.send(message2)
+}
+var wss = new WebSocketServer({server: server});
+
+wss.on("connection", function(ws){
+  ws.on('message', function(message,id) {
+    let data = message.toString()
+    if(data.substring(0,4) == 'USER'){
+     message = message.toString().substring(4,message.length);
+     let arr_ip_id = [
+     ['HydraFire','159.224.183.122'],
+     ['HydraFire','159.224.183.122'],
+     ['HydraFire','46.30.41.26']
+     ]
+     arr_ip_id.some(v=>{
+       if(v[1] == message){ return message = v[0]}else{message = message}
+     })
+     ws['x_user'] = message;
+     somebodyConnected(ws)
+     windowManager.sharedData.set('playing_music','none');
+    }
+    somebodyConnected_log(ws,id,message)
+    
+    
+    if(message.toString().includes('SHUT_UP')){
+      windowManager.sharedData.set('SHUT_UP','true');
+    }else if(message.toString().includes('SPEECH')){
+      windowManager.sharedData.set('SHUT_UP','false');
+    }else{
+      set_to_run(message.toString(),ws);
+    }
+    
+  });
+});
+///////////////////////////////////////////////////////////////////////
+app.on('ready', () => {
+  console.log('server start')
+  ////////////////////////////////////////////////////////////////////////  
+})
---
+++ public/cg.js
@@ -0,0 +1,182 @@
+(function e(t,n,r){
+  function s(o,u){
+    if(!n[o]){
+      if(!t[o]){
+        var a=typeof require=="function" && require;
+        if(!u&&a)return a(o,!0);
+        if(i)return i(o,!0);
+        var f=new Error("Cannot find module '"+o+"'");
+        throw f.code="MODULE_NOT_FOUND",f
+      }
+      var l=n[o]={
+        exports:{}};t[o][0].call(l.exports,function(e){
+          var n=t[o][1][e];
+          return s(n?n:e)},l,l.exports,e,t,n,r)
+    }
+    return n[o].exports
+  }
+  var i=typeof require=="function"&&require;
+  for(var o=0;o<r.length;o++)s(r[o]);
+  return s
+})
+({1:[function(require,module,exports){
+  const Analyser = require('web-audio-analyser')
+  var audio = document.getElementById('audio');
+  
+    
+    
+ 
+
+  var analyser = null;
+  //audioTag
+  //localStorage.audioVolume = 0.45;
+  localStorage.aaaa = 'play';
+  // src
+  // audio.src = 'tracks/Deepierro Remix.mp3'
+  //audio.src = 'tracks/Buku.mp3'
+  //   audio.src = url;
+  //  audio.load();
+
+  audio.crossOrigin = "anonymous";
+  //audio.src = 'tracks/Teminite  Starr Chen - Fireflies.mp3' // src
+
+  //var audio = document.getElementById('audio');
+  // audio.addEventListener('canplay', canplay, false)
+  //audio.crossOrigin = 'Anonymous'
+  //audio.loop = false
+  function pause(){
+    let audio = document.getElementById('audio');
+    audio.stop()
+  }
+
+  audio.load()
+  audio.loop = false;
+  canplay (audio)
+  function canplay (audio) {
+
+    // audio.removeEventListener('canplay', canplay, false)
+    audio.play()
+
+    setTimeout(function () {
+      analyser = Analyser(audio)
+    },100)
+
+
+    var frequencies
+    var waveform
+    setInterval(()=>{
+      if(analyser){
+        frequencies = analyser.frequencies();
+        waveform = analyser.waveform();
+        if(audio.volume != localStorage.audioVolume){
+          audio.volume = localStorage.audioVolume
+        }
+        //localStorage.aaaa = 'play';
+        //console.log(localStorage.aaaa)
+        if(localStorage.aaaa == 'pause'){
+          audio.pause()
+        }else{
+          //audio.play()
+        }
+        draw(waveform,frequencies);
+        //////////////////////////////////////////////////////////////
+      }
+    },15)
+  }
+
+
+},{"./remember":3,"web-audio-analyser":2}],
+  2:[function(require,module,exports){
+    var AudioContext = window.AudioContext || window.webkitAudioContext
+
+    module.exports = WebAudioAnalyser
+
+    function WebAudioAnalyser(audio, ctx, opts) {
+      if (!(this instanceof WebAudioAnalyser)) return new WebAudioAnalyser(audio, ctx, opts)
+      if (!(ctx instanceof AudioContext)) (opts = ctx), (ctx = null)
+
+        opts = opts || {}
+        this.ctx = ctx = ctx || new AudioContext
+
+        if (!(audio instanceof AudioNode)) {
+          audio = (audio instanceof Audio || audio instanceof HTMLAudioElement)
+            ? ctx.createMediaElementSource(audio)
+          : ctx.createMediaStreamSource(audio)
+        }
+
+      this.analyser = ctx.createAnalyser()
+      this.stereo   = !!opts.stereo
+      this.audible  = opts.audible !== false
+      this.wavedata = null
+      this.freqdata = null
+      this.splitter = null
+      this.merger   = null
+      this.source   = audio
+
+      if (!this.stereo) {
+        this.output = this.source
+        this.source.connect(this.analyser)
+        if (this.audible)
+          this.analyser.connect(ctx.destination)
+          } else {
+            this.analyser = [this.analyser]
+            this.analyser.push(ctx.createAnalyser())
+
+            this.splitter = ctx.createChannelSplitter(2)
+            this.merger   = ctx.createChannelMerger(2)
+            this.output   = this.merger
+
+            this.source.connect(this.splitter)
+
+            for (var i = 0; i < 2; i++) {
+              this.splitter.connect(this.analyser[i], i, 0)
+              this.analyser[i].connect(this.merger, 0, i)
+            }
+
+            if (this.audible)
+              this.merger.connect(ctx.destination)
+              }
+    }
+
+    WebAudioAnalyser.prototype.waveform = function(output, channel) {
+      if (!output) output = this.wavedata || (
+        this.wavedata = new Uint8Array((this.analyser[0] || this.analyser).frequencyBinCount)
+      )
+
+      var analyser = this.stereo
+      ? this.analyser[channel || 0]
+      : this.analyser
+
+      analyser.getByteTimeDomainData(output)
+
+      return output
+    }
+
+    WebAudioAnalyser.prototype.frequencies = function(output, channel) {
+      if (!output) output = this.freqdata || (
+        this.freqdata = new Uint8Array((this.analyser[0] || this.analyser).frequencyBinCount)
+      )
+
+      var analyser = this.stereo
+      ? this.analyser[channel || 0]
+      : this.analyser
+
+      analyser.getByteFrequencyData(output)
+
+      return output
+    }
+
+  },{}],3:[function(require,module,exports){
+    module.exports = function (audio) {
+      const t = parseFloat(localStorage.getItem('remember:audioPosition')) || 0
+
+      audio.currentTime = t
+
+      setInterval(function () {
+        localStorage.setItem('remember:audioPosition', String(audio.currentTime))
+      }, 250)
+    }
+
+  },{}]},{},[1]);
+
+
