8e2e331357d7586ea565baecb2aa613268a89830
HydraFire <hydrafire.plus@gmail.com>
2017-07-29 17:03:23 +0300

    test sub_true9

--- public/bundle.js
+++ public/bundle.js
@@ -248,13 +248,13 @@ tBackgroundPrevious = tBackgroundCurrent
 tHighlightPrevious = tHighlightCurrent
 if (true) {
   tForegroundCurrent = palettes[19][0]
-  let r_color = (Math.random()*10)/10|0;
-  let g_color = (Math.random()*10)/10|0;
-  let b_color = (Math.random()*10)/10|0;
+  let r_color = (Math.random()*5)/5|0;
+  let g_color = (Math.random()*5)/5|0;
+  let b_color = (Math.random()*5)/5|0;
 
-  let r_color2 = (Math.random()*20)/10|0;
-  let g_color2 = (Math.random()*20)/10|0;
-  let b_color2 = (Math.random()*20)/10|0;
+  let r_color2 = (Math.random()*10)/5|0;
+  let g_color2 = (Math.random()*10)/5|0;
+  let b_color2 = (Math.random()*10)/5|0;
   tBackgroundCurrent = [r_color, g_color, b_color]
   tHighlightCurrent = [r_color2, g_color2, b_color2]
 } else {
